4aa4fc3035365558d1b8ab300c54f9704a3bbfb3
Fabio A. Gomez Diaz <fdiaz@skillz.com>
2022-02-03 11:05:27 -0600

    - Added new final score scene
    Signed-off-by: Fabio A. Gomez Diaz <fdiaz@skillz.com>

--- unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/FinalScore.cs
+++ unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/FinalScore.cs
@@ -4,21 +4,24 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class FinalScore : MonoBehaviour
 {
+    [SerializeField] private TMP_Text finalScoreText;
     [SerializeField] private TMP_Text highScoreText;
 
-    public const string Scene = "Scene_MainMenu";
+    public const string Scene = "Scene_FinalScore";
 
     private void Start()
     {
+        int finalScore = PlayerPrefs.GetInt(ScoreSystem.FinalScoreKey, 0);
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
+        finalScoreText.text = $"Final Score: {finalScore}";
         highScoreText.text = $"High Score: {highScore}";
     }
 
-    public void Play()
+    public void ViewResults()
     {
-        SceneManager.LoadScene(Car.Scene);
+        SceneManager.LoadScene(MainMenu.Scene);
     }
 }
