e06ac409ce23af0e1ac122b6127f8c158e2cf027
Fabio A. Gomez Diaz <fdiaz@skillz.com>
2022-04-08 12:30:18 -0500

    - Updated Driver-2019 with Progression Scene - Also added Unity Version string

--- unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/CrashlyticsInit.cs
+++ unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/CrashlyticsInit.cs
@@ -9,24 +9,24 @@ public class CrashlyticsInit : MonoBehaviour {
     // Use this for initialization
     void Start () {
         // Initialize Firebase
-        // Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-        //     var dependencyStatus = task.Result;
-        //     if (dependencyStatus == Firebase.DependencyStatus.Available)
-        //     {
-        //         // Create and hold a reference to your FirebaseApp,
-        //         // where app is a Firebase.FirebaseApp property of your application class.
-        //         // Crashlytics will use the DefaultInstance, as well;
-        //         // this ensures that Crashlytics is initialized.
-        //         Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+        /*Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // Crashlytics will use the DefaultInstance, as well;
+                // this ensures that Crashlytics is initialized.
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
 
-        //         // Set a flag here for indicating that your project is ready to use Firebase.
-        //     }
-        //     else
-        //     {
-        //         UnityEngine.Debug.LogError(System.String.Format(
-        //           "Could not resolve all Firebase dependencies: {0}",dependencyStatus));
-        //         // Firebase Unity SDK is not safe to use here.
-        //     }
-        // });
+                // Set a flag here for indicating that your project is ready to use Firebase.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}",dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });*/
     }
 }
--- unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/FinalScore.cs
+++ unity/_game-templates/Driver-2019.4.35f1/Assets/Scripts/FinalScore.cs
@@ -1,3 +1,4 @@
+//using SkillzSDK;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,9 +10,9 @@ public class FinalScore : MonoBehaviour
     [SerializeField] private TMP_Text finalScoreText;
     [SerializeField] private TMP_Text highScoreText;
 
-    public const string Scene = "Scene_FinalScore";
+    public const string Scene = "FinalScore";
     private int finalScore;
-    private int _retrySeconds = 3;
+    private int _retryDelayInSeconds = 3;
 
     private void Start()
     {
@@ -20,33 +21,70 @@ public class FinalScore : MonoBehaviour
 
         finalScoreText.text = $"Final Score: {finalScore}";
         highScoreText.text = $"High Score: {highScore}";
-
-        // Submit Score
-        //TryToSubmitScore(finalScore);
     }
 
     public void ViewResults()
     {
+        Debug.Log("Clicked \"View Results\" Button");
         SceneManager.LoadScene(MainMenu.Scene);
+
         // Go back to Skillz
+        // TryToSubmitScore(finalScore);
+    }
+    /*
+    ////////////////////////////
+    // SUBMIT SCORE TO SKILLZ //
+    ////////////////////////////
+
+    void TryToSubmitScore(int score) {
+        Debug.Log("Submitting score to Skillz!");
+        SkillzCrossPlatform.SubmitScore(score, OnSubmitScoreSuccess, OnSubmitScoreFailure);
+    }
+
+    void OnSubmitScoreSuccess() {
+        Debug.Log("Successfully submitted score to Skillz!");
         //SkillzCrossPlatform.ReturnToSkillz();
+        UpdateCustomPlayerData();
     }
 
-    /*void TryToSubmitScore(int score) {
-        SkillzCrossPlatform.SubmitScore(score, OnSuccess, OnFailure);
+    void OnSubmitScoreFailure(string reason) {
+        Debug.Log($"Fail to submit score to Skillz: {reason}");
+        StartCoroutine(RetrySubmitScore());
     }
 
-    void OnSuccess() {
-        Debug.Log("Success");
-    }
-
-    void OnFailure(string reason) {
-        Debug.LogWarning("Fail: " + reason);
-        StartCoroutine(RetrySubmit());
-    }
-
-    IEnumerator RetrySubmit() {
-        yield return new WaitForSeconds(_retrySeconds);
+    IEnumerator RetrySubmitScore() {
+        Debug.LogWarning($"Retrying to submit score to Skillz in \"{_retryDelayInSeconds}\" seconds");
+        yield return new WaitForSeconds(_retryDelayInSeconds);
         TryToSubmitScore(finalScore);
+    }
+
+    //////////////////////////////////////////////////
+    // UPDATE SKILLZ PROGRESSION CUSTOM PLAYER DATA //
+    //////////////////////////////////////////////////
+
+    void UpdateCustomPlayerData() {
+        // Create Date String
+        string newDate = System.DateTime.Now.ToString();
+
+        Debug.Log($"newDate string: \"{newDate}\"");
+
+        // Create Update Dictionary
+        Dictionary<string, object> updateDict = new Dictionary<string, object>()
+        {
+            { Progression.CPDLastPlayed, newDate }
+        };
+
+        SkillzCrossPlatform.UpdateProgressionUserData(ProgressionNamespace.PLAYER_DATA, updateDict, OnUpdateCustomPlayerDataSuccess, OnUpdateCustomPlayerDataFail);
+    }
+
+    void OnUpdateCustomPlayerDataSuccess()
+    {
+        Debug.Log("Successfully updated Custom Player Data!");
+        SkillzCrossPlatform.ReturnToSkillz();
+    }
+
+    void OnUpdateCustomPlayerDataFail(string reason)
+    {
+        Debug.Log($"Failed to update Custom Player Data: {reason}");
     }*/
 }
